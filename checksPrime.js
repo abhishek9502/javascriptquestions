81852f5f39681199739a442d58cf85941eeb3005
Abhishek Heerekar <abhishek.heerekar@acuvate.com>
2026-08-20 10:29:20 +0530

    day 3 task

---
+++ checksPrime.js
@@ -0,0 +1,12 @@
+function checksPrime(n){
+    if (n<=1){
+        return false;
+    }
+    for (let i=2;i<=n**0.5;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(checksPrime(13));
